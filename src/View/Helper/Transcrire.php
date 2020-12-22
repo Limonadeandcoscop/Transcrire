5f518e10fb74f01e2963c4792cf096f07933f656
Franck Dupont <kyfr59@gmail.com>
2020-12-22 15:03:18 +0100

    displays projects informations

--- src/View/Helper/Transcrire.php
+++ src/View/Helper/Transcrire.php
@@ -11,18 +11,6 @@ class Transcrire extends AbstractHelper
 {
 
     /**
-     * Display project infos area
-     *
-     * @return HTML
-     */
-    public function projectInfos()
-    {
-        $view = $this->getView();
-        $projectId = $view->project->id();
-        return $view->render('project-infos');
-    }
-
-    /**
      * @var RouteMatch
      */
     protected $routeMatch;
@@ -35,4 +23,58 @@ class Transcrire extends AbstractHelper
     {
         $this->routeMatch = $routeMatch;
     }
+
+
+    /**
+     * Display project infos area
+     *
+     * @return HTML
+     */
+    public function projectInfos()
+    {
+        $view = $this->getView();
+
+        $itemSet = $view->project->itemSet();
+
+        $publishers = $subjects = $types = $linkedResources = [];
+
+        if ($publishersValues = $itemSet->value('dcterms:publisher', ['all' => true, 'type' => 'literal'])) {
+            foreach($publishersValues as $publisher) {
+                $publishers[] = $publisher;
+            }
+        }
+
+        if ($subjectsValues = $itemSet->value('dcterms:subject', ['all' => true, 'type' => 'literal'])) {
+            foreach($subjectsValues as $subject) {
+                $subjects[] = $subject;
+            }
+        }
+
+        if ($typesValues = $itemSet->value('dcterms:type', ['all' => true, 'type' => 'literal'])) {
+            foreach($typesValues as $type) {
+                $types[] = $type;
+            }
+        }
+
+        if ($linkedResourcesValues = $itemSet->values()) {
+            foreach($linkedResourcesValues as $value) {
+                foreach($value['values'] as $v) {
+                    if ($v->type() == 'uri') {
+                        $linkedResources[$v->uri()] = (string)$v;
+                    }
+                }
+            }
+        }
+
+        return $view->render(
+            'project-infos',
+            [
+                'project'          => $view->project,
+                'publishers'       => $publishers,
+                'subjects'         => $subjects,
+                'types'            => $types,
+                'linkedResources'  => $linkedResources,
+            ]);
+    }
+
 }
