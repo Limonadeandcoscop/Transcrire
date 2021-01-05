a6d81467618a5c979b4c2500224b984b8d413de8
Franck Dupont <kyfr59@gmail.com>
2021-01-05 18:34:33 +0100

    adding media infos area

--- src/View/Helper/Transcrire.php
+++ src/View/Helper/Transcrire.php
@@ -149,6 +149,62 @@ class Transcrire extends AbstractHelper
 
 
     /**
+     * Display media & item infos area
+     *
+     * @return HTML
+     */
+    public function mediaInfos()
+    {
+        $view = $this->getView();
+
+        $itemSet = $view->project->itemSet();
+
+        $publishers = $subjects = $types = $linkedResources = [];
+
+        if ($publishersValues = $itemSet->value('dcterms:publisher', ['all' => true, 'type' => 'literal'])) {
+            foreach($publishersValues as $publisher) {
+                $publishers[] = $publisher;
+            }
+        }
+
+        if ($subjectsValues = $itemSet->value('dcterms:subject', ['all' => true, 'type' => 'literal'])) {
+            foreach($subjectsValues as $subject) {
+                $subjects[] = $subject;
+            }
+        }
+
+        if ($typesValues = $itemSet->value('dcterms:type', ['all' => true, 'type' => 'literal'])) {
+            foreach($typesValues as $type) {
+                $types[] = $type;
+            }
+        }
+
+        if ($linkedResourcesValues = $itemSet->values()) {
+            foreach($linkedResourcesValues as $value) {
+                foreach($value['values'] as $v) {
+                    if ($v->type() == 'uri') {
+                        $linkedResources[$v->uri()] = (string)$v;
+                    }
+                }
+            }
+        }
+
+        return $view->render(
+            'media-infos',
+            [
+                'item'             => $view->item,
+                'sMedia'           => $view->sMedia,
+                'project'          => $view->project,
+                'publishers'       => $publishers,
+                'subjects'         => $subjects,
+                'types'            => $types,
+                'linkedResources'  => $linkedResources,
+                'itemProgressBar'  => $this->itemProgressBar(),
+            ]);
+    }
+
+
+    /**
      * Display item's progress bar
      *
      * @return HTML
